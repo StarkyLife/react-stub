d4b61f4a148f0ccc8354d98d99323b0ed74d5932
Ilshar Ismagilov <ilshat.ismagilov2014@gmail.com>
2022-12-16 13:03:06 +0300

    chore: inject css as <style></style> into html

--- webpack.base.js
+++ webpack.base.js
@@ -1,32 +1,39 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.bundle.js'
+    path: path.join(__dirname, "dist"),
+    filename: "app.bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         options: {
           presets: [
-            '@babel/preset-env',
-            '@babel/preset-typescript',
-            '@babel/preset-react',
-          ]
-        }
-      }
-    ]
+            "@babel/preset-env",
+            "@babel/preset-typescript",
+            "@babel/preset-react",
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
