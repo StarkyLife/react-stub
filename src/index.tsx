db2b214f7bc0fa5e8b3e4d2e831bff9a4ff776c4
Ilshar Ismagilov <ilshat.ismagilov2014@gmail.com>
2022-12-20 18:56:32 +0300

    chore: add default error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app';
+import DefaultErrorBoundary from './default-error-boundary';
 
 import './styles.css';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <DefaultErrorBoundary>
+    <App />
+  </DefaultErrorBoundary>
+);
